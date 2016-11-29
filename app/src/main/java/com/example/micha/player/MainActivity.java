36502428be4d659d97bac230ae3ce5a0747cccb9
pkwiczala <kwiczala.pawel@gmail.com>
2016-11-29 11:36:35 +0100

    test 2

--- app/src/main/java/com/example/micha/player/MainActivity.java
+++ app/src/main/java/com/example/micha/player/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//test2
+        setContentView(R.layout.activity_main);//test21
 
     }
 }
