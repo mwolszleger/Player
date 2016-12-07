d8986fde2fb6f6586b569d584698509b1888635c
mwolszleger <mwolszleger@interia.pl>
2016-12-07 11:13:59 +0100

    dołączenie brakujących plików

---
+++ app/src/main/java/com/example/micha/player/Player.java
@@ -0,0 +1,195 @@
+package com.example.micha.player;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Player.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Player#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Player extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private OnFragmentInteractionListener mListener;
+
+
+    private MediaPlayer mp;
+    private int soundId;
+    private Button button;
+    private SeekBar seek;
+    private Handler mHandler = new Handler();
+
+
+    public Player() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Player.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Player newInstance(String param1, String param2) {
+        Player fragment = new Player();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+
+
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View rootView=inflater.inflate(R.layout.fragment_player, container, false);
+
+
+        button = (Button) rootView.findViewById(R.id.button3);
+        seek=(SeekBar)rootView.findViewById(R.id.seekBar);
+        if(mp != null) {
+
+            mp.release();
+
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(mp.isPlaying())
+                    mp.pause();
+                else
+                    mp.start();
+
+            }
+        });
+
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                if(fromUser) {
+                    seekBar.setMax(mp.getDuration() / 1000);
+                    //textView.setText(progress + "/" + seekBar.getMax());
+                    mp.seekTo(progress * 1000);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        getActivity().runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+
+
+
+                if(mp != null){
+                    int mCurrentPosition = mp.getCurrentPosition() / 1000;
+                    seek.setProgress(mCurrentPosition);
+                }
+                mHandler.postDelayed(this, 1000);
+            }
+        });
+
+//        soundId=R.raw.aaa;
+//        mp = MediaPlayer.create(getActivity(), soundId);
+
+        // mp.start();
+
+
+
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+    //public void loadSong(jakiśargument)
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+        void previousSong();
+        void nextSong();
+    }
+
+
+}
